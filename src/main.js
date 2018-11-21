ce5b18b8dfa63825327c558a6c118d2ebf185c86
Vanni <1226066980@qq.com>
2018-11-21 10:14:08 +0800

    1.城市选择页面组件

--- src/main.js
+++ src/main.js
@@ -7,10 +7,19 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 import './base.css'
+import CityNav from './components/citynav.vue';
+import HotCity from './components/hotcity.vue';
+import PinYin from './components/pinyin.vue';
+import CityList from './components/citylist.vue';
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+
+Vue.component('citynav',CityNav)
+Vue.component('hotcity',HotCity)
+Vue.component('pinyin',PinYin)
+Vue.component('citylist',CityList)
 
 new Vue({
   el: '#app',
