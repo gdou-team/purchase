2c8681802078e741504e9dec65dfe9da25f01970
Vanni <1226066980@qq.com>
2018-11-22 15:34:15 +0800

    1.大小商品组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-	<citylist />
+	<smallgoods />
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,9 @@ import CityNav from './components/citynav.vue';
 import HotCity from './components/hotcity.vue';
 import PinYin from './components/pinyin.vue';
 import CityList from './components/citylist.vue';
+import Footer from './components/pagefooter.vue';
+import BigGoods from './components/biggoods.vue';
+import SmallGoods from './components/smallgoods.vue';
 
 Vue.config.productionTip = false
 
@@ -20,6 +23,9 @@ Vue.component('citynav',CityNav)
 Vue.component('hotcity',HotCity)
 Vue.component('pinyin',PinYin)
 Vue.component('citylist',CityList)
+Vue.component('pagefooter',Footer)
+Vue.component('biggoods',BigGoods)
+Vue.component('smallgoods',SmallGoods)
 
 new Vue({
   el: '#app',
