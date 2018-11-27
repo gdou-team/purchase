3aa4cb35e1cb38bdac2e58bc1e72da6712057151
c10342 <1376023740@qq.com>
2018-11-27 16:46:16 +0800

    把用户信息存储到localstorage中

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 // 修改用户信息
 export const setUserInfo = (state,userInfo)=>{
     state.userInfo = userInfo
+    window.localStorage.setItem('userInfo',JSON.stringify(userInfo))
 } 
 
 // 修改地理位置
--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,11 @@
+let userInfo = window.localStorage.getItem('userInfo')
+
+userInfo = userInfo ? JSON.parse(userInfo) : {}
+
+
 // 仓库
 let state = {
-    userInfo:{},
+    userInfo:userInfo,
     location:'湛江市广东海洋大学'
 }
 
