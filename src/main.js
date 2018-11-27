a9113f93b47f84df7dea986aa26077f708e62bce
c10342 <1376023740@qq.com>
2018-11-27 19:23:59 +0800

    小bug

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -30,7 +29,6 @@ import ListItem from './components/listItem.vue'
 import OrderList from './components/order_list.vue'
 import Comment from './components/comment.vue'
 import OrderDetails from './components/orderDetails.vue'
-
 import SelectCity from '@/containers/selectCity.vue'
 import UserInfo from '@/containers/userinfo.vue'
 import ChangePhone from '@/containers/changePhone.vue'
@@ -39,51 +37,52 @@ Vue.config.productionTip = false
 
 Vue.use(ElementUI);
 
-Vue.component('MyHeader',Header)
-Vue.component('MySearch',Search)
-Vue.component('citynav',CityNav)
-Vue.component('hotcity',HotCity)
-Vue.component('pinyin',PinYin)
-Vue.component('citylist',CityList)
-Vue.component('SelectCity',SelectCity)
-Vue.component('pagefooter',Footer)
-Vue.component('biggoods',BigGoods)
-Vue.component('smallgoods',SmallGoods)
-Vue.component('goodslist',GoodsList)
-Vue.component('goodsdetail',GoodsDetail)
-Vue.component('orderlist',OrderList)
-Vue.component('orderdetails',OrderDetails)
-
-Vue.component('Nav',Nav)
-Vue.component('Item',Item)
-Vue.component('ShopList',ShopList)
-Vue.component('shopContent',shopContent)
-Vue.component('NavigationElevator',NavigationElevator)
-Vue.component('ListItem',ListItem)
-Vue.component('Loading',Loading)
-Vue.component('orderlist',OrderList)
-Vue.component('UserInfo',UserInfo)
-Vue.component('ChangePhone',ChangePhone)
-Vue.component('Comment',Comment)
+Vue.component('MyHeader', Header)
+Vue.component('MySearch', Search)
+Vue.component('citynav', CityNav)
+Vue.component('hotcity', HotCity)
+Vue.component('pinyin', PinYin)
+Vue.component('citylist', CityList)
+Vue.component('SelectCity', SelectCity)
+Vue.component('pagefooter', Footer)
+Vue.component('biggoods', BigGoods)
+Vue.component('smallgoods', SmallGoods)
+Vue.component('goodslist', GoodsList)
+Vue.component('goodsdetail', GoodsDetail)
+Vue.component('orderlist', OrderList)
+Vue.component('orderdetails', OrderDetails)
+Vue.component('Nav', Nav)
+Vue.component('Item', Item)
+Vue.component('ShopList', ShopList)
+Vue.component('shopContent', shopContent)
+Vue.component('NavigationElevator', NavigationElevator)
+Vue.component('ListItem', ListItem)
+Vue.component('Loading', Loading)
+Vue.component('orderlist', OrderList)
+Vue.component('UserInfo', UserInfo)
+Vue.component('ChangePhone', ChangePhone)
+Vue.component('Comment', Comment)
 
 // 登录权限控制
 router.beforeEach((to, from, next) => {
-  if(!store.userInfo){
+  if (!store.userInfo) {
     console.log('用户还没有登陆')
   }
   next()
 })
 
 // 图片懒加载
-	Vue.use(VueLazyload,{
-  loading:require("@/assets/default.png")
-})*
+Vue.use(VueLazyload, {
+  loading: require("@/assets/default.png")
+})
 
 
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>',
   store
 })
