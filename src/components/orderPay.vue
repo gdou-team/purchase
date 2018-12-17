9f3df8676d9bd121447617bb3853068a3535b8e4
c10342 <1376023740@qq.com>
2018-12-17 20:59:44 +0800

    实现支付功能

--- src/components/orderPay.vue
+++ src/components/orderPay.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="pay-container">
+    <div class="pay-container" ref="payContainer">
         <div class="title">
             请在<span>{{time}}</span>内完成支付，超时会自动取消订单
         </div>
         <div class="detail">
-            <div>项目 ：{{orderPayDetail.}}</div>
+            <div>项目 ：{{orderPayDetail.goods.goodsTitle}}</div>
             <div>应付金额 ： ￥<span>{{orderPayDetail.order.total}}</span></div>
         </div>
         <div class="pay">
@@ -12,20 +12,21 @@
             <div class="num">支付 ￥<span>{{orderPayDetail.order.total}}</span></div>
             <div class="gopay">
                 <div @click="goBack">返回修改订单</div>
-                <el-button :disabled="isOverdue" type="primary">去付款</el-button>
+                <el-button :disabled="isOverdue" type="primary" @click='goToPay'>去付款</el-button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
+import { post } from "../util";
 export default {
   data() {
     return {
       pay: "1",
       time: "00:00:00",
-      isOverdue:false
+      isOverdue: false
     };
   },
   methods: {
@@ -37,39 +38,56 @@ export default {
         let s = parseInt((time / 1000) % 60);
         let m = parseInt((time / 1000 / 60) % 60);
         let h = parseInt(time / 1000 / 60 / 60);
-        if(s<0 || m<0 || h<0){
-            clearInterval(this.timer)
-            this.time = `00:00:00`;
-            this.isOverdue = true
-            return
+        if (s < 0 || m < 0 || h < 0) {
+          clearInterval(this.timer);
+          this.time = `00:00:00`;
+          this.isOverdue = true;
+          this.$router.replace({
+            name: "orderList"
+          });
+          return;
         }
-        if(h<10){
-            h = `0${h}`
+        if (h < 10) {
+          h = `0${h}`;
         }
-        if(m<10){
-            h = `0${m}`
+        if (m < 10) {
+          m = `0${m}`;
         }
-        if(s<10){
-            s = `0${s}`
+        if (m == 0) {
+          m = "00";
+        }
+        if (s < 10) {
+          s = `0${s}`;
         }
         this.time = `${h}:${m}:${s}`;
       }, 1000);
     },
-    goBack(){
-      this.$router.back()
+    goBack() {
+      this.$router.back();
+    },
+    async goToPay() {
+      try {
+        const formData = new FormData();
+        formData.append("orderId", this.orderPayDetail.order.id);
+        const result = await post("/tjsanshao/order/pay", formData);
+        const div = document.createElement("div");
+        div.innerHTML = result; //此处form就是后台返回接收到的数据
+        document.body.appendChild(div);
+        document.forms[0].submit();
+      } catch (error) {}
     }
   },
   mounted() {
     this.timer = null;
     this.setTime();
   },
-  computed:{
-    ...mapGetters(['orderPayDetail'])
+  computed: {
+    ...mapGetters(["orderPayDetail"])
   },
-  beforeDestroy(){
-      if(this.timer){
-          clearInterval(this.timer)
-      }
+  beforeDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 };
 </script>
